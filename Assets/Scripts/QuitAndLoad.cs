aa8bd79963740b11a9c2191c81b30c594b5683e4
Maoyeedy <100255436+Maoyeedy@users.noreply.github.com>
2023-01-19 03:21:52 +0800

    Backup Before WebGL

--- Assets/Scripts/QuitAndLoad.cs
+++ Assets/Scripts/QuitAndLoad.cs
@@ -1,23 +1,25 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class QuitAndLoad : MonoBehaviour
 {
     public KeyCode quit = KeyCode.Escape;
     public KeyCode reload = KeyCode.Return;
     public bool clearScoreOnStart = true;
+    public TextMeshProUGUI scoreText;
 
-    private void Awake()
+    private void Start()
     {
-        if (clearScoreOnStart)
-            GameProgress.PlayerScore = 0;
+        scoreText.text = $"Your Score is {GameProgress.PlayerScore.ToString()}";
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(quit))
             Application.Quit();
-        if (Input.GetKeyDown(reload))
+        if (Input.GetKeyDown(reload) || Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
             GameProgress.PlayerScore = 0;
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);

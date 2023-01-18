45ee660eb6605c288b210d17987e8a9fb84b1a99
Maoyeedy <100255436+Maoyeedy@users.noreply.github.com>
2023-01-19 03:22:31 +0800

    Backup Before WebGL

---
+++ Assets/Scripts/MouseFollower.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class MouseFollower : MonoBehaviour
+{
+    public float speed = 5f;
+    public float rotationSpeed = 25f;
+    private bool _isMobile;
+
+    private void Start()
+    {
+        _isMobile = Application.platform is RuntimePlatform.Android or RuntimePlatform.IPhonePlayer;
+    }
+
+    private void Update()
+    {
+        // if (_isMobile)
+        // {
+        //     if(Input.touchCount > 0 && Input.GetTouch(0).phase is TouchPhase.Stationary or TouchPhase.Moved)
+        //         MouseFollowAndRotate();
+        // }
+        // else
+        // {
+            if (Input.touchCount > 0 && Input.GetTouch(0).phase is TouchPhase.Stationary or TouchPhase.Moved || Input.GetMouseButton(0))
+                MouseFollowAndRotate();
+        // }
+    }
+
+    private void MouseFollowAndRotate()
+    {
+        Vector2 mousePos = Camera.main!.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = Vector2.Lerp(transform.position, mousePos, speed * Time.deltaTime);
+        var position = transform.position;
+        var angle = Mathf.Atan2(mousePos.y - position.y, mousePos.x - position.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, angle), Time.deltaTime * rotationSpeed);
+    }
+}
---
+++ Assets/Scripts/MouseRotate.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class MouseRotate : MonoBehaviour
+{
+    
+    public bool isAndroid;
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            // Get the mouse position in screen space
+
+        }
+    }
+
+
+}
